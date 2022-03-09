7b6e6b2bebfe4386c55185698b0cd4a539d09124
iMAC <sergio.pereo@outlook.com>
2022-03-08 19:36:38 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+
+import SearchAppBar from './components/navigation/SearchAppBar';
+import Forecast from './components/weather/Forecast';
+
+import Typography from '@mui/material/Typography';
+import { ThemeProvider, useTheme, createTheme } from '@mui/material/styles';
+
 import './App.css';
+import axios from 'axios'
 
 function App() {
+
+
+  const OPEN_WEATHER_BASE_URL = "https://api.openweathermap.org/data/2.5/onecall"
+  const RESERVEMOS_BASE_URL = "https://search.reservamos.mx/api/v2/places"
+  const API_KEY = process.env.REACT_APP_API_KEY
+  const [dailyWeather, setDailyWeather] = useState([])
+  const [current, setCurrent] = useState(null)
+  const [query, setQuery] = useState("")
+  const [isQuering, setIsQuering] = useState(false)
+  const [direction, setDirection] = useState("")
+
+  const handleSearch = async () => {
+    if(!isQuering){
+      try{
+        setIsQuering(true)
+
+        const coords = await axios.get(RESERVEMOS_BASE_URL, {
+          params: {
+            q: query
+          }
+        })
+        setDirection(`${coords.data[0].country}, ${coords.data[0].state}, ${coords.data[0].city_name}.`)
+        const weather = await axios.get(OPEN_WEATHER_BASE_URL, {
+          params: {
+            lat: coords.data[0].lat,
+            lon: coords.data[0].long,
+            exclude: "hourly,minutely",
+            units: "metric",
+            appid: API_KEY
+          }
+        })
+        setDailyWeather(weather.data.daily)
+        setCurrent(weather.data.current)
+        setIsQuering(false)
+        console.log(coords.data[0])
+        console.log(weather)
+        console.log(weather.data)
+      } catch(error){
+        setIsQuering(false)
+        console.log(error)
+      }
+    } else {
+      console.log("Is quering... wait!")
+    }
+  }
+
+  const handleKeyDown = (event) => {
+    if(event.key === 'Enter'){
+      if(query.length > 0){
+        handleSearch()
+      }
+    }
+  }
+
+  const handleOnChange = (event) => {
+    setQuery(event.target.value)
+  }
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={darkTheme}>
+        <SearchAppBar query={query} handleOnChange={handleOnChange} handleKeyDown={handleKeyDown} />
+        <Typography variant="h5" gutterBottom component="div">
+          {direction}
+        </Typography>
+        <Forecast current={current} dailyWeather={dailyWeather} />
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/components/weather/Forecast.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+
+const MONTH_LABELS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+
+function Forecast (props){
+    const CURRENT_DATE = props.current !== null ? new Date(props.current.dt * 1000) : 0;
+    return (
+        <Box style={{marginTop: "40px"}} sx={{ flexGrow: 1 }}>
+            <Grid container spacing={3} justifyContent="space-between" style={{margin: "15px"}} columns={{ xs: 4, sm: 8, md: 12 }}>
+                {props.current !== null ? (
+                    <Grid key={props.current.dt} item xs={4} sm={8} md={12} zeroMinWidth container columns={{ xs: 4, sm: 8, md: 12 }}>
+                        <Paper style={{width: "100%"}}>
+                            <Grid item xs={4} sm={8} md={12} container columns={{ xs: 6, sm: 8, md: 12 }}>
+                                <Grid item xs={6} sm={8} md={12}>
+                                    <Typography>Temperature now</Typography>
+                                </Grid>
+                                <Grid item xs={6} sm={8} md={12}>
+                                    <Typography>{`${props.current.temp} °C`}</Typography>
+                                </Grid>
+                            </Grid>
+                            <Grid item xs={4} sm={8} md={12}>
+                                <Typography>{`${MONTH_LABELS[CURRENT_DATE.getMonth()]} ${CURRENT_DATE.getDate()}, ${CURRENT_DATE.getFullYear()}`}</Typography>
+                            </Grid>
+                        </Paper>
+                    </Grid>
+                ):(
+                    <div/>
+                )}
+                {
+                    props.dailyWeather.map(day => {
+                        const DATE = new Date(day.dt * 1000);
+                        return(
+                            <Grid key={day.dt} item xs={2} sm={4} md={6} zeroMinWidth container columns={{ xs: 4, sm: 8, md: 12 }}>
+                                <Paper style={{width: "100%"}}>
+                                    <Grid item xs={4} sm={8} md={12} container columns={{ xs: 6, sm: 8, md: 12 }}>
+                                        <Grid item xs={6} sm={8} md={12}>
+                                            <Typography>Forecast temperature</Typography>
+                                        </Grid>
+                                        <Grid item xs={3} sm={4} md={6}>
+                                            <Typography>Min</Typography>
+                                        </Grid>
+                                        <Grid item xs={3} sm={4} md={6}>
+                                            <Typography>Max</Typography>
+                                        </Grid>
+                                        <Grid item xs={3} sm={4} md={6}>
+                                            <Typography>{`${day.temp.min} °C`}</Typography>
+                                        </Grid>
+                                        <Grid item xs={3} sm={4} md={6}>
+                                            <Typography>{`${day.temp.max} °C`}</Typography>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid item xs={4} sm={8} md={12}>
+                                        <Typography>{`${MONTH_LABELS[DATE.getMonth()]} ${DATE.getDate()}, ${DATE.getFullYear()}`}</Typography>
+                                    </Grid>
+                                </Paper>
+                            </Grid>
+                        )
+                    })
+                }
+            </Grid>
+        </Box>
+    );
+}
+
+export default Forecast;
